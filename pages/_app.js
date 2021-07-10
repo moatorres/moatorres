0cfc73be804fadfa46c66097169779af2085e199
Moa Torres <44585769+moatorres@users.noreply.github.com>
2021-07-10 14:17:18 -0300

    update: added next/head in _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import Head from 'next/head'
 
 const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+  return (
+    <Fragment>
+      <Head>
+        <title>Moa Torres – Portfolio</title>
+      </Head>
+      <Component {...pageProps} />
+    </Fragment>
+  )
 }
 
 export default App
