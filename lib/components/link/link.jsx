89a436b648cf4710840823efe420949cad029769
Moa Torres <44585769+moatorres@users.noreply.github.com>
2021-07-11 01:06:32 -0300

    new: link component

---
+++ lib/components/link/link.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { useTheme } from '../themes'
+
+const Link = React.forwardRef(
+  ({ href, decorated, children, className, block, ...props }, ref) => {
+    const theme = useTheme()
+    const padding = block ? theme.layout.gapQuarter : '0'
+    const decoration = decorated ? 'underline' : 'none'
+
+    return (
+      <a className={`link ${className}`} href={href} {...props} ref={ref}>
+        {children}
+        <style jsx>{`
+          .link {
+            display: inline-flex;
+            align-items: baseline;
+            line-height: inherit;
+            color: ${theme.colors.link};
+            text-decoration: none;
+            padding: calc(${padding} * 0.8) calc(${padding} * 1.7);
+            border-radius: ${block ? theme.layout.radius : 0};
+            width: fit-content;
+            transition: color 200ms ease 0ms;
+          }
+          .link:hover,
+          .link:active,
+          .link:focus {
+            text-decoration: ${decoration};
+          }
+          .link:hover {
+            color: ${theme.colors.success.light};
+          }
+        `}</style>
+      </a>
+    )
+  }
+)
+
+Link.defaultProps = {
+  href: '#',
+  color: false,
+  underline: false,
+  block: false,
+  className: '',
+}
+
+export default React.memo(Link)
