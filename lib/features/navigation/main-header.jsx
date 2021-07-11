1e44f0d7278f6fe64fefddc230d3b2e10a03e3a7
Moa Torres <44585769+moatorres@users.noreply.github.com>
2021-07-11 01:08:29 -0300

    update: added active link logic

--- lib/features/navigation/main-header.jsx
+++ lib/features/navigation/main-header.jsx
@@ -1,16 +1,33 @@
 import React from 'react'
-import Header from '../../components/header'
+import { useRouter } from 'next/router'
+import Header from '@components/header'
+import Avatar from '@components/avatar'
+
+const menu = [
+  { title: 'Blog', path: '/blog' },
+  { title: 'About', path: '/about' },
+  { title: 'Home', path: '/' },
+]
 
 const MainHeader = () => {
+  const router = useRouter()
+
   return (
     <Header>
       <Header.Item>
-        <Header.Link href="/">Moa</Header.Link>
+        <Header.Link href="/">
+          <Avatar src="https://avatars.githubusercontent.com/u/44585769?v=4" />
+        </Header.Link>
       </Header.Item>
       <Header.Item style={{ justifyContent: 'flex-end' }}>
-        <Header.Link href="/posts">Blog</Header.Link>
-        <Header.Link href="#">About</Header.Link>
-        <Header.Link href="#">Home</Header.Link>
+        {menu.map((item, index) => (
+          <Header.Link
+            id={index}
+            href={item.path}
+            active={router.pathname === item.path}>
+            {item.title}
+          </Header.Link>
+        ))}
       </Header.Item>
     </Header>
   )
