39b8991291947289e9d665be31e73c79a883aaea
Moa Torres <44585769+moatorres@users.noreply.github.com>
2021-07-11 22:49:12 -0300

    update: about page on root pages dir

---
+++ pages/about.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Page from '@components/page'
+import MainHeader from '@features/navigation/main-header'
+
+const About = () => {
+  return (
+    <Page size="mini">
+      <MainHeader />
+      <Page.Content>
+        <h1>About</h1>
+      </Page.Content>
+    </Page>
+  )
+}
+
+export default About
