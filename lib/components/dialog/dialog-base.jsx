4d94ec37c940802cfd869a3cb69beab93e22467a
Moa Torres <44585769+moatorres@users.noreply.github.com>
2021-07-14 12:49:06 -0300

    fix: auto margin

--- lib/components/dialog/dialog-base.jsx
+++ lib/components/dialog/dialog-base.jsx
@@ -17,14 +17,14 @@ const DialogBase = ({ children, ...props }) => {
           z-index: 999;
           margin: 10vh auto;
           background-color: ${theme.colors.background};
-          width: 440px;
+          width: 40vw;
           outline: none;
           transition: all 0.2s ease;
         }
         @media screen and (max-width: ${theme.breakpoints.sm.min}) {
           .dialog-base {
             width: 100vw;
-            margin: auto auto 0 auto;
+            margin: 10vh auto 0 auto;
             max-height: 80vh;
             bottom: 0;
             height: calc(100vh - calc(30px + (3 * ${theme.layout.gap})));
