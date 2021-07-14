7b32c029d3538c76231bc68561be7c1b62fc9c5e
Moa Torres <44585769+moatorres@users.noreply.github.com>
2021-07-14 19:13:24 -0300

    update: added color and go home btn

--- pages/404.js
+++ pages/404.js
@@ -1,14 +1,21 @@
 import React from 'react'
+import { useRouter } from 'next/router'
 import Page from '@components/page'
+import Button from '@components/button'
 import MainHeader from '@features/navigation/main-header'
 
 const NotFound = () => {
+  const router = useRouter()
   return (
     <Page size="mini" title="404 — Moa Torres">
       <MainHeader />
       <Page.Content>
-        <h1 style={{ fontSize: '6rem' }}>404</h1>
+        <h1 style={{ fontSize: '6rem', lineHeight: '1', color: '#6146d8' }}>
+          404
+        </h1>
         <h1>Page Not Found</h1>
+        <p>The page you are looking for doesn't exist or has been moved.</p>
+        <Button onClick={() => router.push('/')}>Go Home</Button>
       </Page.Content>
     </Page>
   )
