d5fdc2b372c58600f7481192e840aaf1bca9eb20
Moa Torres <44585769+moatorres@users.noreply.github.com>
2021-07-14 21:25:58 -0300

    update: link colors

--- pages/404.js
+++ pages/404.js
@@ -1,21 +1,30 @@
 import React from 'react'
 import { useRouter } from 'next/router'
+import { useTheme } from '@components/themes'
 import Page from '@components/page'
 import Button from '@components/button'
 import MainHeader from '@features/navigation/main-header'
 
 const NotFound = () => {
   const router = useRouter()
+  const theme = useTheme()
   return (
     <Page size="mini" title="404">
       <MainHeader />
       <Page.Content>
-        <h1 style={{ fontSize: '6rem', lineHeight: '1', color: '#6146d8' }}>
+        <h1
+          style={{
+            fontSize: '6rem',
+            lineHeight: '1',
+            color: theme.colors.link,
+          }}>
           404
         </h1>
         <h1>Page Not Found</h1>
-        <p>The page you are looking for doesn't exist or has been moved.</p>
-        <Button onClick={() => router.push('/')}>Go Home</Button>
+        <p style={{ color: theme.colors.link }}>
+          The page you are looking for doesn't exist or has been moved.
+        </p>
+        <Button onClick={() => router.push('/')}>← Go Home</Button>
       </Page.Content>
     </Page>
   )
